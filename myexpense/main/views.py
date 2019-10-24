b9e61377a2c389df9eee2716caeeb92efd2dabee
Sharad Joshi <sharadjoshi@QAIT.COM>
2019-10-24 12:35:04 +0530

    Thirs commit - added whats next feature

--- myexpense/main/views.py
+++ myexpense/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from .models import Detail
 
@@ -8,34 +8,60 @@ def index(request):
 
 
 def entry(request):
-    return render(request, 'index.html')
+    defaultUsers = {'Anand Chauhan': 'Anand Chauhan', 'Aman Johri': 'Aman Johri', 'Anil Kumar Sahu': 'Anil Kumar Sahu', 'Prashant Jain': 'Prashant Jain',
+                    'Rahul Chakraborty': 'Rahul Chakraborty', 'Sharad Joshi': 'Sharad Joshi'}
+    users = {}
+    send = {}
+    last_five = Detail.objects.all().order_by('-date_deposite')[:5]
+    print(type(last_five))
+    for ls in last_five:
+        users[ls.user_name] = ls.user_name
+
+    sorted_users = sorted(users.values())
+    sorted_defaultusers = sorted(defaultUsers.values())
+    print(sorted_defaultusers)
+    print(sorted_users)
+
+    diff = set(sorted_defaultusers) - set(sorted_users)
+    print(diff)
+    print(type(diff))
+
+    for i in diff:
+        send[i] = i
+
+    print(send)
+
+    return render(request, 'index.html', {'name':list(send.keys())[0]})
+
 
 def search(request):
-      qst = Detail.objects.all().order_by('-date_deposite')
+    qst = Detail.objects.all().order_by('-date_deposite')
 
-      if request.method == 'POST':     
-            date_min = request.POST['fromDate']
-            date_max = request.POST['toDate']
-          
-            if is_valid_queryparam(date_min):
-                  qst = qst.filter(date_deposite__gte=date_min)
+    if request.method == 'POST':
+        date_min = request.POST['fromDate']
+        date_max = request.POST['toDate']
 
-            if is_valid_queryparam(date_max):
-                  qst = qst.filter(date_deposite__lte=date_max)
-                  
-      return render_to_response('ajax_search.html', {'query' : qst})
+        if is_valid_queryparam(date_min):
+            qst = qst.filter(date_deposite__gte=date_min)
+
+        if is_valid_queryparam(date_max):
+            qst = qst.filter(date_deposite__lte=date_max)
+
+    return render_to_response('ajax_search.html', {'query': qst})
+
 
 def add_amount(request):
 
-      if request.method == 'POST':
-            user = request.POST['user']
-            amount = request.POST['amount']
-            date = request.POST['date']
+    if request.method == 'POST':
+        user = request.POST['user']
+        amount = request.POST['amount']
+        date = request.POST['date']
 
-            qs = Detail(user_name=user, amount=amount, date_deposite=date)
-            saveToDB(qs)
+        qs = Detail(user_name=user, amount=amount, date_deposite=date)
+        saveToDB(qs)
 
-            return HttpResponse('')
+        return HttpResponse('')
+
 
 def saveToDB(query):
     query.save()
